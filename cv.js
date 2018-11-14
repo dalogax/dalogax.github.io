7c09110926c5d693f92cad064bbb1f9285ac8d12
dalogax <dalogax@gmail.com>
2018-11-15 00:22:33 +0100

    initial jsoncv

---
+++ cv.js
@@ -0,0 +1,48 @@
+var cv = {
+    name: "Daniel",
+    surname: "López García",
+    nickname: "dalogax",
+    birth: 610675200,
+    contact: {
+        email: "dalogax@gmail.com"
+    },
+    website: "http://www.dalogax.com",
+    github: "https://github.com/dalogax",
+    expierience: [
+        {
+            title: "Tech Lead",
+            company: "zara.com",
+            startDate: 2015
+        },
+        {
+            title: "Software Engineer",
+            company: "Indra",
+            startDate: 2011,
+            endDate: 2015
+        }
+    ],
+    education: [
+        {
+            degree: "Master in Computer Engineering",
+            school: "University of A Coruña",
+            startDate: 2014
+        },
+        {
+            degree: "Degree in Computer Engineering",
+            school: "University of A Coruña",
+            startDate: 2012,
+            endDate: 2014
+        },
+        {
+            degree: "Technical engineering on computer systems",
+            school: "University of A Coruña",
+            startDate: 2011,
+            endDate: 2008
+        }
+    ],
+    social: {
+        facebook: "https://www.facebook.com/dalogax",
+        twitter: "https://twitter.com/dalogax",
+        instagram: "https://www.instagram.com/dalogax"
+    }
+}
